76fe2443c7b2848e2d8145312340b95e9ed23fcb
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-02-05 15:34:29 -0500

    manually merged android_setup

---
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.mvp.scrumtious.scrumtious;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
