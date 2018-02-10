33971cbeb6cc58b17cda62f1007be60a0226cabb
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-02-10 17:31:43 -0500

    [#154908775] added a password check validation function

--- app/src/main/java/ca/mvp/scrumtious/scrumtious/utils/UserInputValidator.java
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/utils/UserInputValidator.java
@@ -9,11 +9,17 @@ import java.util.regex.Pattern;
 
 public class UserInputValidator {
 
+    private static final int MAX_EMAIL_LENGTH = 254;
+    private static final int MIN_PASSWORD_LENGTH = 8;
+    private static final int MAX_PASSWORD_LENGTH = 254;
+
     // https://stackoverflow.com/questions/8204680/java-regex-email
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-    private static final int MAX_EMAIL_LENGTH = 254;
 
+    // https://stackoverflow.com/questions/11780022/regex-in-java-for-password-validation
+    private static final Pattern PASSWORD_CRITERIA_REGEX =
+            Pattern.compile("^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9\\s]).{8,}");
 
     /**
      * Checks if the email is valid
@@ -31,4 +37,21 @@ public class UserInputValidator {
 
         return -3;
     }
+
+    /**
+     * Checks if the password meets the valid password criteria
+     * @param passwordToCheck String, the password to check
+     * @return -1 on null password, -2 on invalid password length, -3 for invalid password (including
+     * length), 0 for valid password
+     */
+    public static int meetsPasswordCriteria(String passwordToCheck) {
+        if (passwordToCheck == null) return -1;
+        if (passwordToCheck.length() > MAX_PASSWORD_LENGTH || passwordToCheck.length() < MIN_PASSWORD_LENGTH) return -2;
+
+        Matcher matcher = PASSWORD_CRITERIA_REGEX.matcher(passwordToCheck);
+        if (matcher.find()) return 0;
+
+        return -3;
+    }
+
 }
