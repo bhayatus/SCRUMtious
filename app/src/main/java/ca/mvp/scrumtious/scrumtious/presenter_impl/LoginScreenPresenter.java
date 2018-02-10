69e80effb69da8e72f4fe412b1ee5fde1e673a6f
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-02-10 16:53:55 -0500

    [#154908775] implemented the base login screen presenter and login screen prestenter interface

--- app/src/main/java/ca/mvp/scrumtious/scrumtious/presenter_impl/LoginScreenPresenter.java
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/presenter_impl/LoginScreenPresenter.java
@@ -1,5 +1,44 @@
 package ca.mvp.scrumtious.scrumtious.presenter_impl;
 
 
-public class LoginScreenPresenter {
+import android.app.Activity;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import ca.mvp.scrumtious.scrumtious.interfaces.presenter_int.LoginScreenPresenterInt;
+
+public class LoginScreenPresenter implements LoginScreenPresenterInt{
+
+    private FirebaseAuth firebaseAuth;
+    private LoginScreenView loginScreenView;
+
+    public LoginScreenPresenter(LoginScreenView loginScreenView) {
+        this.firebaseAuth = FirebaseAuth.getInstance();
+        this.loginScreenView = loginScreenView;
+    }
+
+    @Override
+    public void attemptLogin(Activity context, String emailAddress, String password) {
+        firebaseAuth = FirebaseAuth.getInstance();
+        // Attempt the sign in
+        firebaseAuth.signInWithEmailAndPassword(emailAddress, password).addOnCompleteListener(context,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) loginScreenView.onSuccessfulLogin();
+                        else loginScreenView.loginExceptionMessage("Invalid login credentials");
+                    }
+                });
+    }
+
+    @Override
+    public void signOut() {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            FirebaseAuth.getInstance().signOut();
+        }
+    }
 }
