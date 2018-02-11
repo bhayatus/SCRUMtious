0080736629557e16752546e430418f6685becdd3
Swetha Maramganty <swethamaramganty@gmail.com>
2018-02-11 18:05:47 -0500

    [#154908800] Task #3: Created method goToLogin in ScreenView

--- app/src/main/java/ca/mvp/scrumtious/scrumtious/interfaces/view_int/CreateProjectScreenViewInt.java
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/interfaces/view_int/CreateProjectScreenViewInt.java
@@ -3,4 +3,5 @@ package ca.mvp.scrumtious.scrumtious.interfaces.view_int;
 public interface CreateProjectScreenViewInt {
     void createProjectExceptionMessage(String error);
     void onSuccessfulCreateProject();
+    void goToLogin();
 }
