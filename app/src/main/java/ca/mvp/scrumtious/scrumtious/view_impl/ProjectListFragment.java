8f5b84eb1c284d0dc4b39959a3e3ea45f95519b8
Usaim <usaimbhayat@gmail.com>
2018-02-11 12:48:06 -0500

    Setup fragments and activities for project list screen

--- app/src/main/java/ca/mvp/scrumtious/scrumtious/view_impl/ProjectListFragment.java
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/view_impl/ProjectListFragment.java
@@ -1,12 +1,16 @@
 package ca.mvp.scrumtious.scrumtious.view_impl;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import ca.mvp.scrumtious.scrumtious.R;
 import ca.mvp.scrumtious.scrumtious.interfaces.view_int.ProjectListViewInt;
@@ -40,5 +44,32 @@ public class ProjectListFragment extends Fragment implements ProjectListViewInt{
     @Override
     public void goToProjectScreen(String pid) {
 
+    }
+
+    public static class ProjectsViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        TextView titleView, ownerEmailAddressView, descriptionView;
+        Button projectRowButton;
+
+        public ProjectsViewHolder(View itemView) {
+            super(itemView);
+            this.mView = itemView;
+
+            titleView = (TextView) mView.findViewById(R.id.projectListTitle);
+            ownerEmailAddressView = (TextView) mView.findViewById(R.id.projectListOwner);
+            descriptionView = (TextView) mView.findViewById(R.id.projectListDesc);
+            projectRowButton = (Button) mView.findViewById(R.id.projectListButton);
+        }
+
+        public Button getButtonInProjectRow(){
+            return projectRowButton;
+        }
+
+        // Populates each row of the recycler view with the project details
+        public void setDetails(Context context, String title, String ownerEmailAddress, String description){
+            titleView.setText(title);
+            ownerEmailAddressView.setText(ownerEmailAddress);
+            descriptionView.setText(description);
+        }
     }
 }
