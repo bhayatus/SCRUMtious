6eac77f94d0d09f4d7939d84e2f50d2f9957defd
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-02-11 15:44:22 -0500

    [#154908800] added project POJO implementation

--- app/src/main/java/ca/mvp/scrumtious/scrumtious/model/Project.java
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/model/Project.java
@@ -5,4 +5,38 @@ package ca.mvp.scrumtious.scrumtious.model;
  */
 
 public class Project {
+
+    private String projectTitle;
+    private String projectOwnerEmail;
+    private String projectDesc;
+
+    private final int MAX_DESC_SIZE = 254;
+
+    // empty constructor is needed for firebase
+    public Project() {
+
+    }
+
+    public Project(String projectTitle, String projectOwnerEmail, String projectDesc) {
+
+        if (projectTitle == null || projectOwnerEmail == null || projectDesc == null) return;
+
+        this.projectTitle = projectTitle;
+        this.projectOwnerEmail = projectOwnerEmail;
+        this.projectDesc = projectDesc;
+    }
+
+    public String getProjectTitle() { return this.projectTitle; }
+    public String getProjectDesc() { return this.projectDesc; }
+    public String getProjectOwnerEmail() { return this.projectOwnerEmail; }
+
+    public int setProjectDesc(String newProjectDescToSet) {
+        if (newProjectDescToSet == null) return -3;
+
+        if (newProjectDescToSet.length() > MAX_DESC_SIZE || newProjectDescToSet.length() <= 0) return -2;
+        else {
+            this.projectDesc = newProjectDescToSet;
+            return 0;
+        }
+    }
 }
