4f3ceb76d8a1064ad65d2b55bf10156ae0936b53
Muhammad waleed <muhammad.waleed@mail.utoronto.ca>
2018-02-11 14:08:35 -0500

    [#154908805] project list screen presenter initiatied and implemented

---
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/interfaces/presenter_int/projectListScreenPresenterInt.java
@@ -0,0 +1,20 @@
+package ca.mvp.scrumtious.scrumtious.interfaces.presenter_int;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
+import ca.mvp.scrumtious.scrumtious.model.Project;
+import ca.mvp.scrumtious.scrumtious.view_impl.ProjectListScreenFragment;
+
+/**
+ * Created by muham on 2018-02-11.
+ */
+
+public interface projectListScreenPresenterInt {
+    void setupAuthenticationListener();
+    FirebaseRecyclerAdapter<Project, ProjectListScreenFragment.ProjectsViewHolder> setupAdapter(Context appContext, RecyclerView projectList, ProgressDialog loadingProjectsDialog);
+    void signOut();
+}
---
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/presenter_impl/projectListScreenPresenter.java
@@ -0,0 +1,80 @@
+package ca.mvp.scrumtious.scrumtious.presenter_impl;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import ca.mvp.scrumtious.scrumtious.R;
+import ca.mvp.scrumtious.scrumtious.interfaces.presenter_int.projectListScreenPresenterInt;
+import ca.mvp.scrumtious.scrumtious.interfaces.view_int.ProjectListScreenViewInt;
+import ca.mvp.scrumtious.scrumtious.model.Project;
+import ca.mvp.scrumtious.scrumtious.view_impl.ProjectListScreenFragment;
+
+/**
+ * Created by muham on 2018-02-11.
+ */
+
+public class projectListScreenPresenter implements projectListScreenPresenterInt {
+    private ProjectListScreenViewInt viewProjectsScreenView;
+    private FirebaseAuth mAuth;
+    private DatabaseReference rootRef;
+    private Query mQuery;
+    public projectListScreenPresenter (ProjectListScreenViewInt viewProjectsScreenView){
+        this.viewProjectsScreenView = viewProjectsScreenView;
+        this.mAuth = FirebaseAuth.getInstance();
+    }
+    @Override
+    public void setupAuthenticationListener() {
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() == null){
+            //replace that with the method
+            viewProjectsScreenView.returnToLoginScreen();
+        }
+    }
+
+    @Override
+    public FirebaseRecyclerAdapter<Project, ProjectListScreenFragment.ProjectsViewHolder> setupAdapter(final Context appContext, RecyclerView projectList, ProgressDialog loadingProjectsDialog) {
+        final ProgressDialog dialog = loadingProjectsDialog;
+        //just to be safe if constructor wasn't called before
+        mAuth = FirebaseAuth.getInstance();
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        String userID = mAuth.getCurrentUser().getUid();
+        mQuery = rootRef.child("projects").orderByChild(userID).equalTo("true");
+        FirebaseRecyclerAdapter<Project, ProjectListScreenFragment.ProjectsViewHolder> projectListAdapter
+                = new FirebaseRecyclerAdapter<Project, ProjectListScreenFragment.ProjectsViewHolder>(
+                Project.class,
+                R.layout.fragment_view_projects_list_item,
+                ProjectListScreenFragment.ProjectsViewHolder.class,
+                mQuery
+        ) {
+            protected void populateViewHolder(ProjectListScreenFragment.ProjectsViewHolder viewHolder, Project model, final int position) {
+                viewHolder.setDetails(appContext, model.getProjectTitle(), model.getProjectOwnerEmail(), model.getProjectDesc());
+                final String pid = getRef(position).getKey();
+
+                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        viewProjectsScreenView.goToProjectScreen(pid);
+                    }
+                });
+            }
+        };
+        return null;
+    }
+
+    @Override
+    public void signOut() {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            FirebaseAuth.getInstance().signOut();
+            viewProjectsScreenView.returnToLoginScreen();
+        }
+    }
+}
+
