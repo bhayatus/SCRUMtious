1f201564f1152cbcf9ff121966de6094e0efa489
Swetha Maramganty <swethamaramganty@gmail.com>
2018-02-11 16:40:07 -0500

    [#154908800] completed task 3

---
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/interfaces/view_int/CreateProjectScreenViewInt.java
@@ -0,0 +1,6 @@
+package ca.mvp.scrumtious.scrumtious.interfaces.view_int;
+
+public interface CreateProjectScreenViewInt {
+    void createProjectExceptionMessage(String error);
+    void onSuccessfulCreateProject();
+}
