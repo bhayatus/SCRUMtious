895a792a256236beea0ce460d0e908ba83c21a01
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-02-20 14:01:01 -0500

    [#155159077] created the UserStoryAcitivty and interface, and hooked it up with the intial presenter which will be implemented soon

---
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/interfaces/view_int/CreateUserStoryViewInt.java
@@ -0,0 +1,10 @@
+package ca.mvp.scrumtious.scrumtious.interfaces.view_int;
+
+/**
+ * Created by badsh on 2/20/2018.
+ */
+
+public interface CreateUserStoryViewInt {
+    void showMessage(String message);
+    void onClickCreateUserStory();
+}
--- app/src/main/java/ca/mvp/scrumtious/scrumtious/view_impl/CreateUserStoryActivity.java
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/view_impl/CreateUserStoryActivity.java
@@ -1,19 +1,43 @@
 package ca.mvp.scrumtious.scrumtious.view_impl;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import ca.mvp.scrumtious.scrumtious.R;
+import ca.mvp.scrumtious.scrumtious.interfaces.view_int.CreateUserStoryViewInt;
 
-public class CreateUserStoryActivity extends AppCompatActivity {
+public class CreateUserStoryActivity extends AppCompatActivity implements CreateUserStoryViewInt {
+
+    private CreateUserStoryPresenterInt createUserStoryPresenter;
+    private String pid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user_story);
+
+        Bundle data = getIntent().getExtras();
+        pid = data.getString("projectId");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        createUserStoryPresenter = new CreateUserStoryPresenter(this, pid);
     }
 
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    @Override
+    public void showMessage(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClickCreateUserStory() {
+
+    }
 }
