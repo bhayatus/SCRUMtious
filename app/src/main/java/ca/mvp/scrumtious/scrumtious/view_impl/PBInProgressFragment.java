adfb96a6f78f8c6b7ff42a7f66cb30768785f49d
Usaim <usaimbhayat@gmail.com>
2018-02-20 13:05:11 -0500

    [#155159077] added initial files to get the product backlog tabs working

---
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/view_impl/PBInProgressFragment.java
@@ -0,0 +1,32 @@
+package ca.mvp.scrumtious.scrumtious.view_impl;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ca.mvp.scrumtious.scrumtious.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PBInProgressFragment extends Fragment {
+
+
+    public PBInProgressFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_pbin_progress, container, false);
+
+        return view;
+    }
+
+}
