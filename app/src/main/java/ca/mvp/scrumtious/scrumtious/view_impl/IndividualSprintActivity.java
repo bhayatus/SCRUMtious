5005f47f606049db2842c4c80befcccdda4b8400
Nensi Deliana <nensi.deliana@mail.utoronto.ca>
2018-02-25 19:37:01 -0500

    [#155159166] Finished View Sprint Backlog

--- app/src/main/java/ca/mvp/scrumtious/scrumtious/view_impl/IndividualSprintActivity.java
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/view_impl/IndividualSprintActivity.java
@@ -1,6 +1,5 @@
 package ca.mvp.scrumtious.scrumtious.view_impl;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,75 +9,43 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageButton;
 
 import ca.mvp.scrumtious.scrumtious.R;
-import ca.mvp.scrumtious.scrumtious.interfaces.presenter_int.ProductBacklogPresenterInt;
-import ca.mvp.scrumtious.scrumtious.interfaces.view_int.ProductBacklogViewInt;
-import ca.mvp.scrumtious.scrumtious.presenter_impl.ProductBacklogPresenter;
-import ca.mvp.scrumtious.scrumtious.utils.AuthenticationHelper;
 
-public class ProductBacklogActivity extends AppCompatActivity implements ProductBacklogViewInt {
+public class IndividualSprintActivity extends AppCompatActivity {
 
-    private ProductBacklogPresenterInt productBacklogPresenter;
     private SectionsPagerAdapter mSectionsPagerAdapter;
-    private String pid;
     private ViewPager mViewPager;
-
+    private String pid, sid;
+    private TabLayout tabs;
     private DrawerLayout mDrawerLayout;
     private NavigationView navigationView;
-
     private boolean alreadyDeleted;
-
-    private ImageButton logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_backlog);
-
-        alreadyDeleted = false; // Project is not deleted at this point
-
+        setContentView(R.layout.activity_individual_sprint);
         Bundle data = getIntent().getExtras();
         pid = data.getString("projectId");
-        this.productBacklogPresenter = new ProductBacklogPresenter(this, pid);
+        sid = data.getString("sprintId");
 
-        // In case project is deleted, the user has to be taken back to project list screen
-        productBacklogPresenter.setupProjectDeletedListener();
-
-        logoutBtn = findViewById(R.id.productBacklogLogoutBtn);
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AuthenticationHelper.logout(ProductBacklogActivity.this);
-            }
-        });
-
+        tabs = (TabLayout) findViewById(R.id.individualSprintTabs);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.individualSprintNavDrawer);
+        navigationView = (NavigationView) findViewById(R.id.individualSprintNavView);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.productBacklogViewPager);
+        mViewPager = (ViewPager) findViewById(R.id.individualSprintViewPager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.productBacklogTabs);
-        tabLayout.setupWithViewPager(mViewPager);
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-
-        // The following sets up the navigation drawer
-        mDrawerLayout = findViewById(R.id.productBacklogNavDrawer);
-        navigationView = findViewById(R.id.productBacklogNavView);
-
-        // By default, should highlight product backlog option to indicate that is where the user is
         navigationView.setCheckedItem(R.id.nav_product_backlog);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
@@ -98,7 +65,7 @@ public class ProductBacklogActivity extends AppCompatActivity implements Product
                                 handler.postDelayed(new Runnable() {
                                     @Override
                                     public void run() {
-                                        Intent intent = new Intent(ProductBacklogActivity.this, IndividualProjectActivity.class);
+                                        Intent intent = new Intent(IndividualSprintActivity.this, IndividualProjectActivity.class);
                                         intent.putExtra("projectId", pid);
                                         startActivity(intent);
                                         finish();
@@ -108,6 +75,19 @@ public class ProductBacklogActivity extends AppCompatActivity implements Product
                                 break;
                             // User chooses product backlog, do nothing as we are already there
                             case R.id.nav_product_backlog:
+                                // Allow nav drawer to close smoothly before switching activities
+                                handler = new Handler();
+                                delayMilliseconds = 250;
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Intent intent = new Intent(IndividualSprintActivity.this, ProductBacklogActivity.class);
+                                        intent.putExtra("projectId", pid);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                },delayMilliseconds);
+
                                 break;
 
                             case R.id.nav_sprints:
@@ -116,7 +96,7 @@ public class ProductBacklogActivity extends AppCompatActivity implements Product
                                 handler.postDelayed(new Runnable() {
                                     @Override
                                     public void run() {
-                                        Intent intent = new Intent(ProductBacklogActivity.this, SprintListActivity.class);
+                                        Intent intent = new Intent(IndividualSprintActivity.this, SprintListActivity.class);
                                         intent.putExtra("projectId", pid);
                                         startActivity(intent);
                                         finish();
@@ -130,48 +110,15 @@ public class ProductBacklogActivity extends AppCompatActivity implements Product
                 });
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.productBacklogToolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.individualSprintToolbar);
         setSupportActionBar(toolbar);
         // Sets icon for menu on top left
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
     }
 
-    // Used when the menu icon is clicked to open the navigation drawer
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            // User clicks on the menu icon on the top left
-            case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);  // OPEN DRAWER
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onClickAddUserStory(View view){
-        Intent intent = new Intent(ProductBacklogActivity.this, CreateUserStoryActivity.class);
-        intent.putExtra("projectId", pid);
-        startActivity(intent);
-    }
-
-    // If project no longer exists while we are on this screen, must return to the project list screen
-    @Override
-    public void onProjectDeleted() {
-
-        // DELETED NORMALLY FLAG PREVENTS THIS FROM TRIGGERING AGAIN AFTER ALREADY BEING DELETED
-        if (!alreadyDeleted) {
-            alreadyDeleted = true;
-
-            // Return to project list screen, and clear the task stack so we can't go back
-            Intent intent = new Intent(ProductBacklogActivity.this, ProjectTabsActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
-        }
-    }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
@@ -182,28 +129,35 @@ public class ProductBacklogActivity extends AppCompatActivity implements Product
         @Override
         public Fragment getItem(int position) {
             switch(position){
+
                 case 0:
+                    SprintOverviewFragment sprintOverviewFragment = new SprintOverviewFragment();
+                    return sprintOverviewFragment;
+
+                case 1:
                     Bundle data = new Bundle();
                     data.putString("projectId", pid);
+                    data.putString("sprintId", sid);
 
                     // This fragment will display info for product backlog in progress user stories
-                    data.putString("type", "PB_IN_PROGRESS");
+                    data.putString("type", "SPRINT_IN_PROGRESS");
 
                     // Passes in null, to tell fragment and presenter that this is a pb user story
-                    data.putString("sprintId", "null");
+                    data.putString("sprintId", sid);
 
                     BacklogFragment pbInProgressFragment = new BacklogFragment();
                     pbInProgressFragment.setArguments(data);
                     return pbInProgressFragment;
-                case 1:
+                case 2:
                     data = new Bundle();
                     data.putString("projectId", pid);
+                    data.putString("sprintId", sid);
 
                     // This fragment will display info for product backlog completed user stories
-                    data.putString("type", "PB_COMPLETED");
+                    data.putString("type", "SPRINT_COMPLETED");
 
                     // Passes in null, to tell fragment and presenter that this is a pb user story
-                    data.putString("sprintId", "null");
+                    data.putString("sprintId", sid);
 
                     BacklogFragment pbCompletedFragment = new BacklogFragment();
                     pbCompletedFragment.setArguments(data);
@@ -215,8 +169,8 @@ public class ProductBacklogActivity extends AppCompatActivity implements Product
 
         @Override
         public int getCount() {
-            // Show 2 total pages.
-            return 2;
+            // Show 3 total pages.
+            return 3;
         }
 
         @Nullable
@@ -224,9 +178,12 @@ public class ProductBacklogActivity extends AppCompatActivity implements Product
         public CharSequence getPageTitle(int position) {
             switch(position){
                 case 0:
-                    return "IN PROGRESS";
+                    return "OVERVIEW";
 
                 case 1:
+                    return "IN PROGRESS";
+
+                case 2:
                     return "COMPLETED";
 
                 default:
@@ -235,10 +192,4 @@ public class ProductBacklogActivity extends AppCompatActivity implements Product
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(ProductBacklogActivity.this, ProjectTabsActivity.class);
-        startActivity(intent);
-        finish();
-    }
 }
