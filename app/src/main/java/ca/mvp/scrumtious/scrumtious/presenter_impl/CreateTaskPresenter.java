cf69a5d0430399255950231dc39a8a609f8b7d80
Nensi Deliana <nensi.deliana@mail.utoronto.ca>
2018-03-17 13:01:51 -0400

    [#155159184] Finished Task #2

--- app/src/main/java/ca/mvp/scrumtious/scrumtious/presenter_impl/CreateTaskPresenter.java
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/presenter_impl/CreateTaskPresenter.java
@@ -1,17 +1,74 @@
 package ca.mvp.scrumtious.scrumtious.presenter_impl;
 
 
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import ca.mvp.scrumtious.scrumtious.interfaces.presenter_int.CreateTaskPresenterInt;
 import ca.mvp.scrumtious.scrumtious.interfaces.view_int.CreateTaskViewInt;
 
-public class CreateTaskPresenter implements CreateTaskPresenterInt {
+public class CreateTaskPresenter extends AppCompatActivity implements CreateTaskPresenterInt {
 
     private CreateTaskViewInt createTaskView;
     private String pid, usid;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
 
     public CreateTaskPresenter(CreateTaskViewInt createTaskView, String pid, String usid){
         this.createTaskView = createTaskView;
         this.pid = pid;
         this.usid = usid;
+    }
+
+    public void addTaskToDatabase(final String taskDesc){
+
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference().child("projects").child(this.pid).child("user_stories")
+                .child(this.usid);
+
+        final String taskId = mRef.push().getKey(); //unique key for tasks
+
+        mRef.child("numTasks").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                long numTasks = (long) dataSnapshot.getValue();
+                numTasks++;
+
+                Map taskMap = new HashMap<>();
+                taskMap.put("/tasks/" + taskId + "/taskDesc", taskDesc);
+                taskMap.put("/tasks/" + taskId + "/assignedTo", null);
+                taskMap.put("/" + "numTasks", numTasks);
+
+                mRef.updateChildren(taskMap).addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            createTaskView.onSuccessfulCreateTask();
+                        } else {
+                            createTaskView.showMessage("An error occurred, failed to create" +
+                                    " task.", false);
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
