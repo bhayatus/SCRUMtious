3b545a0f53fb9fbdace6aa89ee46eebd7b0bc084
Swetha Maramganty <swethamaramganty@gmail.com>
2018-03-18 14:02:11 -0400

    [#155159207] Adding interface for presenter

---
+++ app/src/main/java/ca/mvp/scrumtious/scrumtious/interfaces/presenter_int/GroupChatPresenterInt.java
@@ -0,0 +1,11 @@
+package ca.mvp.scrumtious.scrumtious.interfaces.presenter_int;
+
+/**
+ * Created by Swetha on 2018-03-18.
+ */
+
+public interface GroupChatPresenterInt {
+
+    void addMessagesToDatabase(String message, long timeStamp);
+
+}
